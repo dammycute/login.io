3460ebebe521c6cbd5f870f2d36753b3e2495a81
dammycute <58667901+dammycute@users.noreply.github.com>
2020-11-26 03:21:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+function msg(){
+      			alert("Hurray! You have succesfully registered your account.")
+      		}
+      		
+      		/*var check = function() { if (+IAs-document+IAs.getElementById(+IAs'password'+IAs).value == document+IAs.getElementById(+IAs'confirm_password'+IAs).value) { document+IAs.getElementById(+IAs'message'+IAs).style.color = 'green'; document+IAs.getElementById(+IAs'message'+IAs).innerHTML = 'matching'; } else { document+IAs.getElementById(+IAs'message'+IAs).style.color = 'red'; document+IAs.getElementById(+IAs'message'+IAs).innerHTML = 'not matching'; } }*/
+      		
